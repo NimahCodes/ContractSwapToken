ca79759af17655c3ca1c576d21bca8d1a6713f9f
FOLAKEMI UMAR <folake.umar@gmail.com>
2022-08-17 20:11:27 +0100

    token swap

--- contracts/LinkToken.sol
+++ contracts/LinkToken.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract LinkToken is ERC20 {
+    uint amount2;
     constructor() ERC20("LinkToken", "LNK") {
-        _mint(_msgSender(), 10000e18);
+        _mint(_msgSender(), amount2);
     }
 }
--- contracts/Web3Token.sol
+++ contracts/Web3Token.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract W3BToken is ERC20 {
-
+contract Web3BridgeToken is ERC20 {
+    uint amount1;
     constructor() ERC20("W3BToken", "W3B") {
-        _mint(_msgSender(), 10000e18);
+        _mint(_msgSender(), amount1);
     }
 }
